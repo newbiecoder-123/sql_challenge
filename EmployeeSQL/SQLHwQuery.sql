9f92cf775e9ca0689be36f1108a26aeaf4528fc4
Nghia Nguyen <nghianguyen214@gmail.com>
2020-08-02 10:20:41 -0700

    completed hw

--- EmployeeSQL/SQLHwQuery.sql
+++ EmployeeSQL/SQLHwQuery.sql
@@ -37,9 +37,24 @@ FROM employees
 WHERE first_name LIKE 'Hercules' AND last_name LIKE 'B%';
 
 --6. List all employees in the Sales department, including their employee number, last name, first name, and department name.
-
+SELECT e.emp_no, e.last_name, e.first_name, dm.dept_name
+FROM employees e
+INNER JOIN dept_emp d ON e.emp_no = d.emp_no
+INNER JOIN departments dm ON dm.dept_no = d.dept_no
+WHERE dept_name = 'Sales';
 
 --7. List all employees in the Sales and Development departments, including their employee number, last name, first name, and department name.
-
+SELECT e.emp_no, e.last_name, e.first_name, dm.dept_name
+FROM employees e
+INNER JOIN dept_emp d ON e.emp_no = d.emp_no
+INNER JOIN departments dm ON dm.dept_no = d.dept_no
+WHERE dept_name = 'Sales' OR dept_name = 'Development';
 
 --8. In descending order, list the frequency count of employee last names, i.e., how many employees share each last name.
+SELECT last_name, count(last_name) AS frequency_lastname
+FROM employees e
+GROUP BY last_name
+ORDER BY last_name DESC;
+
+
+
